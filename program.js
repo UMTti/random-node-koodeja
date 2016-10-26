7fb5ad229be4c03dab7a0ba85d8ed898df41ef21
Pihla Toivanen <toivanenpihla@gmail.com>
2016-10-26 15:40:53 +0300

    juggling async

--- program.js
+++ program.js
@@ -1,15 +1,55 @@
 var http = require('http');
- var bl = require('bl');
+var bl = require('bl');
 
-http.get(process.argv[2], function(response) {
+var url1 = process.argv[2];
+var url2 = process.argv[3];
+var url3 = process.argv[4];
+var arr = [];
+
+http.get(url1, function(response) {
   response.setEncoding("utf8");
   response.pipe(bl(function (err, data) { 
   	var str = data.toString();
-  	console.log(str.length);
-  	console.log(str);
+  	arr[0] = str;
+  	if(arr[0] != null && arr[1] != null && arr[2] != null){
+  		console.log(arr[0]);
+  		console.log(arr[1]);
+  		console.log(arr[2]);
+  	}
   }));
 }).on('error', function(e) {
   console.log("Got error: " + e.message);
 });
+
+http.get(url2, function(response) {
+  response.setEncoding("utf8");
+  response.pipe(bl(function (err, data) { 
+  	var str = data.toString();
+  	arr[1] = str;
+  	if(arr[0] != null && arr[1] != null && arr[2] != null){
+  		console.log(arr[0]);
+  		console.log(arr[1]);
+  		console.log(arr[2]);
+  	}
+  }));
+}).on('error', function(e) {
+  console.log("Got error: " + e.message);
+});
+
+http.get(url3, function(response) {
+  response.setEncoding("utf8");
+  response.pipe(bl(function (err, data) { 
+  	var str = data.toString();
+  	arr[2] = str;
+  	if(arr[0] != null && arr[1] != null && arr[2] != null){
+  		console.log(arr[0]);
+  		console.log(arr[1]);
+  		console.log(arr[2]);
+  	}
+  }));
+}).on('error', function(e) {
+  console.log("Got error: " + e.message);
+});
+
 
 
