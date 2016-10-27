87b82820da4e208c1d474e74351ef5e24813433c
Pihla Toivanen <toivanenpihla@gmail.com>
2016-10-27 22:48:16 +0300

    api

--- program.js
+++ program.js
@@ -1,12 +1,33 @@
 var http = require('http'); 
 var fs = require('fs');
 var map = require('through2-map');
+var moment = require('moment')
 
-var server = http.createServer(function (request, response) {  
-	 request.pipe(map(function (chunk) {  
+//2013-08-10T12:10:15.474Z
 
-       return chunk.toString().toUpperCase() 
-     })).pipe(response)  
+function parseDate(urli){
+	 var stri = urli.split("?");
+     var klockan = stri[1].split("=")[1];
+     var d = new Date(klockan);
+     return d;
+}
+var server = http.createServer(function (req, response) {  
+  if(req.url.includes("/api/parsetime")) {
+     var d = parseDate(req.url);
+
+	 response.setHeader('Content-Type', 'application/json');
+     response.write(JSON.stringify({ "hour": d.getHours(), "minute": d.getMinutes(), "second":d.getSeconds() }));
+     response.end();
+     return;
+  }
+
+  if(req.url.includes("/api/unixtime")) {
+    var d = parseDate(req.url);
+
+	response.write(JSON.stringify({ "unixtime": d.getTime() }));
+    response.end();
+    return;
+  }
 
 })  
-server.listen(process.argv[2])  
+server.listen(Number(process.argv[2]))  
